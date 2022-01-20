0495f6ec3b57a8b4fccd24e3a5b5ffe2a5e3e4f3
wnghks727 <88995857+wnghks727@users.noreply.github.com>
2022-01-20 17:31:03 +0900

    accommodation service, mapper, controller

--- src/main/java/com/hoge/controller/HomeController.java
+++ src/main/java/com/hoge/controller/HomeController.java
@@ -9,7 +9,7 @@ public class HomeController {
 	
 	@GetMapping("/")
 	public String home(Model model) throws Exception {
-		model.addAttribute("message", "홈페이지 방문을 환영합니다.");
+		// model.addAttribute("message", "홈페이지 방문을 환영합니다.");
 		
 		return "home";
 	}
