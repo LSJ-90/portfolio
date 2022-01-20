8d11f3c2c3ff673c42bca32b2354aa2b419195d0
LSJ-90 <lsj7641148@naver.com>
2022-01-20 17:30:30 +0900

    navbar_demo setting, login에 관련된 쿼리,메서드명 정의

--- src/main/java/com/hoge/mapper/UserMapper.java
+++ src/main/java/com/hoge/mapper/UserMapper.java
@@ -6,8 +6,17 @@ import org.apache.ibatis.annotations.Mapper;
 
 import com.hoge.vo.other.User;
 
+/**
+ * 유저 기능에 관한 메서드명을 정의하였다.
+ * @author 이승준
+ *
+ */
 @Mapper
 public interface UserMapper {
 	
 	List<User> getAllUsers();
+	User getUser();
+	User getUserByNo(int no);
+	User getUserById(String id);
+	User getUserBypwd(String pwd);
 }
--- src/main/webapp/WEB-INF/tiles/header.jsp
+++ src/main/webapp/WEB-INF/tiles/header.jsp
@@ -1,3 +1,3 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
-<p style="border: 2px solid green">테스트 헤더구역 입니다.</p>
+<p style="border: 2px solid blue">테스트222 헤더 구역 입니다.</p>
